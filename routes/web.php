2dc0cbc189361792b19d8f48c37b8644bace2428
endo_wataru <greenstar284@gmail.com>
2021-05-27 18:40:58 +0900

    prfile.blade.phpをログインリンク、ログアウトリンク表示させるよう変更
    web.phpのprofile/createとprofile/editにアクセスした時にリダイレクトするよう変更

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,13 @@ Route::get('/', function () {
 });
 //ルートのグループ(adminを最初につけてね)
 Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 
-  Route::get('profile/create', 'Admin\ProfileController@add');
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
 
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
